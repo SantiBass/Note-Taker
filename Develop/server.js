c479eeb7d311fc0e2e0088021467c498a29f345f
SantiBass <sibarraryan@gmail.com>
2021-12-14 20:32:54 -0800

    working on url host error

--- Develop/server.js
+++ Develop/server.js
@@ -2,26 +2,24 @@
   
 const express = require('express');
 const fs = require('fs');
-const {note} = require("./db/db.json");
+const path = require('path');
+ const note = require("./db/db.json");
 const url = require('./public/routes/urlRoute.js');
 const dbRoute = require('./public/routes/dbRoute.js')
 const PORT  = process.env.PORT ||  3005;
-
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-// app.get('/', (req, res) => res.send('Visit http://localhost:`${PORT}`/api'));
-// GET request for note
-app.get('/api/note', (req, res)=>{
-    res.json(`${req.method} request received to get note`)
-    console.info(`${req.method}request recieived to get note`)
-});
+
+ app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+ );
+
+// // GET request for note
+//  app.get('/api', (req, res)=>{
+//      res.json(`${note} request received to get note`)
+//     console.info(`${req.method}request recieived to get note`)
+// });
 app.listen(PORT, ()=> {
     console.log(`Server is running at http://localhost:${PORT}🚀`)
 });
