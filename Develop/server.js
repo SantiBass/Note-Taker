3e0ca93f0c7a264153b18b7581d8ca46ce571b76
SantiBass <sibarraryan@gmail.com>
2021-12-14 21:19:06 -0800

    working on db route file

--- Develop/server.js
+++ Develop/server.js
@@ -4,16 +4,23 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
  const note = require("./db/db.json");
-const url = require('./public/routes/urlRoute.js');
-const dbRoute = require('./public/routes/dbRoute.js')
+const url = require('./routes/urlRoute');
+const dbRoute = require('./routes/dbRoute');
 const PORT  = process.env.PORT ||  3005;
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', url);
+app.use(express.static('public'));
+app.use('/api', dbRoute);
 
 
- app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
- );
+
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, './public/index.html'))
+//  );
 
 // // GET request for note
 //  app.get('/api', (req, res)=>{
