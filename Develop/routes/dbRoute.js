87e300e4b9d8a688c595ade0da4ab7f3637ad324
SantiBass <sibarraryan@gmail.com>
2021-12-15 15:00:51 -0800

    working on db route to delete  the notes

--- Develop/routes/dbRoute.js
+++ Develop/routes/dbRoute.js
@@ -1,12 +1,28 @@
 const fs = require("fs");
 const app = require("express").Router();
+const {v1:uuidv1} = require("uuid");
+
 app.get("/notes", (req, res)=>{
     var data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
     res.json(data);
 });
 app.post("/notes", (req, res)=>{
-    var data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
+    const newNote = req.body;
+    newNote.id = uuidv1()
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
+    data.push(newNote)
+    fs.writeFileSync("./db/db.json", JSON.stringify(data))
     res.json(data);
 }
-)
+);
+
+ app.delete("/api/notes/:id", (req, res)=>{
+    res.send('delete'+ req.params.id)
+//  data.filter((note)=> note.id !== req.params.id)
+    // const {id} = req.params
+
+    
+    
+   
+ });
 module.exports = app;
--- Develop/server.js
+++ Develop/server.js
@@ -16,7 +16,7 @@ app.use('/', url);
 app.use(express.static('public'));
 app.use('/api', dbRoute);
 
-
+// app.delete()
 
 // app.get('/', (req, res) =>
 //   res.sendFile(path.join(__dirname, './public/index.html'))
