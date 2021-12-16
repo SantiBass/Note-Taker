509e724664d00fa68e60c088e941eb8cfba7a3e2
SantiBass <sibarraryan@gmail.com>
2021-12-16 11:20:25 -0800

    db route to delete done

---
+++ routes/dbRoute.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const app = require("express").Router();
+const { v1: uuidv1 } = require("uuid");
+
+app.get("/notes", (req, res) => {
+    var data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
+    res.json(data);
+});
+
+app.post("/notes", (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv1()
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
+    data.push(newNote)
+    fs.writeFileSync("./db/db.json", JSON.stringify(data));
+    res.json(data);
+});
+
+app.delete("/notes/:id", (req, res) => {
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8", (err) => {
+        if (err) throw err
+    }))
+    console.log(data)
+    console.log("ABOVE IS 23 BELOW IS 28")
+let newData = data.filter(note => note.id != req.params.id)
+
+   fs.writeFile("./db/db.json",  JSON.stringify(newData), (err) => {
+    if (err) throw err
+});
+    console.log(data.filter(note => note.id != req.params.id))
+
+    // notes.splice(req.params.id, 1);
+    // updatedb();
+    // const {id} = req.params
+
+
+
+
+});
+module.exports = app;
